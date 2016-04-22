f0344aae5302796420acacd26afb22be44978ef0
xzjqx <xzjqx@outlook.com>
2016-04-22 12:46:57 +0800

    TCP/IP编程示例

---
+++ TCP-IP/server_demo.py
@@ -0,0 +1,23 @@
+import time, socket, threading
+
+def tcplink(sock, addr):
+    print 'Accept new connection from %s:%s...' % addr
+    sock.send('Welcome!')
+    while True:
+        data = sock.recv(1024)
+        time.sleep(1)
+        if data == 'exit' or not data:
+            break
+        sock.send('Hello, %s!' % data)
+    sock.close()
+    print 'Connection from %s:%s closed.' % addr
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('127.0.0.1', 9999))
+s.listen(5)
+print 'Waiting for connection...'
+
+while True:
+    sock, addr = s.accept()
+    t = threading.Thread(target=tcplink, args=(sock, addr))
+    t.start()
