9448391aaa38eb656049f748288424c41c97f678
xzjqx <xzjqx@outlook.com>
2016-04-26 16:33:19 +0800

    0000

--- show-me-the-code/0000/add-num.py
+++ show-me-the-code/0000/add-num.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
-# 第 0000 题：将你的 QQ 头像（或者微博头像）右上角加上红色的数字，类似于微信未读信息数量那种提示效果
 from PIL import Image, ImageDraw, ImageFont
+
 def add_num(picture, num):
     img = Image.open(picture)
     x, y = img.size
